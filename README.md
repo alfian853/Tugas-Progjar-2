1c5ddf15e05e67a72b18ca5a191fad424686bfc8
alfian <alfian853@gmail.com>
2019-03-24 20:47:09 +0700

    upload tugas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tugas-Progjar-2
 UDP Picture Server
+
+## Usage :
+- aktifkan server, "python server.py"
+- aktifkan 3 client di folder "client_[nomor_client]", masing-masing memiliki code yang sama kecuali port
+- masukkan nama file yang ingin dikirim di console server
+- file terkirim
---
+++ server.py
@@ -0,0 +1,81 @@
+import socket
+import sys
+import os
+import time
+import threading
+
+
+class Client:
+	def __init__(self, address):
+		self.address = address
+
+
+class ServerSenderWorker(threading.Thread):
+	def __init__(self,server_sock, file_name, clients):
+		self.clients = clients
+		self.file_name = file_name
+		self.sock = server_sock
+		threading.Thread.__init__(self)
+
+	def run(self):
+		info = "{\"file_name\" : \"" + self.file_name + "\"}"
+		content = open(self.file_name, 'rb')
+		for client in self.clients:
+			self.sock.sendto(bytes(info), client.address)
+
+		for data in content:
+			for client in self.clients:
+				self.sock.sendto(bytes(data), client.address)
+			time.sleep(0.000001)
+
+		print('File has been sent to all clients')
+
+
+class ServerConnectionWorker(threading.Thread):
+	def __init__(self,clients):
+		self.the_clients = clients
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		threading.Thread.__init__(self)
+
+	def getSock(self):
+		return self.sock
+
+	def run(self):
+		self.sock.bind(('127.0.0.1', 9000))
+
+		while True:
+			data, client_address = self.sock.recvfrom(1024)
+			print('get request from '+str(client_address))
+			if data.decode('utf-8') == 'init_connection':
+				duplicate = False
+				for client in self.the_clients:
+					if client.address == client_address:
+						duplicate = True
+						break
+
+				if not duplicate:
+					self.the_clients.append(Client(client_address))
+					print('New client :', client_address)
+
+			self.sock.sendto(bytes('accepted'), client_address)
+
+
+class Server(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+
+	def run(self):
+		clients = []
+		connection_worker = ServerConnectionWorker(clients)
+		connection_worker.start()
+		while True:
+			file_name = raw_input('Please input the file name >')
+			ServerSenderWorker(connection_worker.getSock(), file_name, clients).start()
+
+def main():
+	svr = Server()
+	svr.start()
+
+if __name__=="__main__":
+	main()
+
